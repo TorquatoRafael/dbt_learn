fcd5f4c2c52f64d621c30273cec671fe01426462
Rafael Torquato <rafastorquato@hotmail.com>
2026-03-04 16:18:37 -0300

    Executando a formatação com as Rules

--- models/staging/jaffe_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffe_shop/stg_jaffle_shop__customers.sql
@@ -1,9 +1,9 @@
- with customers as (   
+with customers as (
     select
-        id as customer_id,
-        first_name,
-        last_name
+        id as customer_id
+        , first_name
+        , last_name
     from {{ source('jaffle_shop', 'customers') }}
-    )
+)
 
-    select * from customers
+select * from customers
